a9bf0a853e4d4523ade3e55ece56ef4b98562e1a
Betsy Tsoi <tsoisybetsy@gmail.com>
2023-07-29 17:40:36 +0900

    Refactor navbar codes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,22 +2,30 @@ export default function NavBar() {
   return(
     <nav id="NavBar">
       <ul>
-        <a href="#Intro">
-          <li className="about">
-          </li>
-        </a>
-        <a href="#Skills">
-          <li className="skill">
-          </li>
-        </a>
-        <a href="#Works">
-          <li className="work">
-          </li>
-        </a>
-        <a href="#Contact">
-          <li className="contact">
-          </li>
-        </a>
+        <li className="about">
+          <a href="#Intro">
+            <span className="desktop">ABOUT ME</span>
+            <span className="mobile">ABOUT</span>
+          </a>
+        </li>
+        <li className="skill">
+          <a href="#Skills">
+            <span className="desktop">SKILLS & TOOLS</span>
+            <span className="mobile">SKILLS</span>
+          </a>
+        </li>
+        <li className="work">
+          <a href="#Works">
+            <span className="desktop">MY WORK</span>
+            <span className="mobile">WORK</span>
+          </a>
+        </li>
+        <li className="contact">
+          <a href="#Contact">
+            <span className="desktop">CONTACT ME</span>
+            <span className="mobile">CONTACT</span>
+          </a>
+        </li>
       </ul>
     </nav>
   );
