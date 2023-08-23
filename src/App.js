3e7a192c92120b0751024f1a583e89012a1c4eaf
Betsy Tsoi <tsoisybetsy@gmail.com>
2023-08-23 23:32:29 +0900

    Implement scroll effect on navbar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,40 @@ import Skills from "./components/Skills";
 import Works from "./components/Works";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import{ useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+
+  const [current, setCurrent] = useState("about");
+  
+  useEffect(() => {
+
+    const about = document.querySelector('#Intro');
+    const skill = document.querySelector('#Skills');
+    const footer = document.querySelector('#Footer')
+
+    const handleScroll = () => {
+      if (window.scrollY + window.innerHeight >= footer.offsetTop) {
+        setCurrent("contact");
+      } else if (window.scrollY > (skill.offsetTop * 2 + skill.offsetHeight) / 2) {
+        setCurrent("work");
+      } else if (window.scrollY > (about.offsetHeight) / 2) {
+        setCurrent("skill");
+      } else {
+        setCurrent("about");
+      };
+    }
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <NavBar />
+      <NavBar current={current} />
       <div className="main">
         <Intro />
         <Skills />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,32 @@
-export default function NavBar() {
+export default function NavBar({ current }) {
+
+  let aboutClass = `about${current === "about" ? " active" : ""}`;
+  let skillClass = `skill${current === "skill" ? " active" : ""}`;
+  let workClass = `work${current === "work" ? " active" : ""}`;
+  let contactClass = `contact${current === "contact" ? " active" : ""}`;
+
   return(
     <nav id="NavBar">
       <ul>
-        <li className="about">
+        <li className={aboutClass}>
           <a href="#Intro">
             <span className="desktop">ABOUT ME</span>
             <span className="mobile">ABOUT</span>
           </a>
         </li>
-        <li className="skill">
+        <li className={skillClass}>
           <a href="#Skills">
             <span className="desktop">SKILLS & TOOLS</span>
             <span className="mobile">SKILLS</span>
           </a>
         </li>
-        <li className="work">
+        <li className={workClass}>
           <a href="#Works">
             <span className="desktop">MY WORK</span>
             <span className="mobile">WORK</span>
           </a>
         </li>
-        <li className="contact">
+        <li className={contactClass}>
           <a href="#Contact">
             <span className="desktop">CONTACT ME</span>
             <span className="mobile">CONTACT</span>
